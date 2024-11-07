e67376704ee052377f5dd3152475d087b138e733
aliyzbsi <aliyzbsi68@hotmail.com>
2024-11-07 17:46:13 +0300

    .

--- src/pages/ToDoListItems.jsx
+++ src/pages/ToDoListItems.jsx
@@ -130,8 +130,8 @@ function ToDoListItems({
 
   return (
     <div className="container mx-auto p-4">
-      <div className="flex flex-col gap-2 justify-center md:flex-row">
-        {["Gün", "Hafta", "Ay", "Yıl", "Hepsi"].map((label) => (
+      <div className="flex flex-col flex-wrap gap-2 justify-center md:flex-row">
+        {["Bugün", "Bu Hafta", "Bu Ay", "Bu Yıl", "Hepsi"].map((label) => (
           <button
             key={label}
             className="bg-blue-500 text-white rounded-full px-4 py-2 transition duration-200 hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"

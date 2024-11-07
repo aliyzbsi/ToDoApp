debbdd7039cd8a4ff5635236b6cfce93be16f91d
aliyzbsi <aliyzbsi68@hotmail.com>
2024-11-07 17:30:07 +0300

    .

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+
 import bg from "../../public/assets/pexels-suzyhazelwood-1629236.jpg";
 function MainLayout() {
   return (
     <div
       className="h-full "
       style={{
-        backgroundImage: `${bg}`,
-        backgroundSize: "cover", // Arka planın tam kapsaması için
-        backgroundPosition: "center", // Ortalanmış pozisyon
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       <Header />
